72224118d4fed1607e9d349637e290c566693c9a
shannuchowdary1992 <127686646+shannuchowdary1992@users.noreply.github.com>
2025-06-09 12:21:32 +0530

    updated app-test.js file

--- app-test.js
+++ app-test.js
@@ -3,7 +3,6 @@ let server = require("./app");
 let chai = require("chai");
 let chaiHttp = require("chai-http");
 
-
 // Assertion 
 chai.should();
 chai.use(chaiHttp); 
@@ -55,6 +54,7 @@ describe('Planets API Suite', () => {
                 done();
               });
         });
+
         it('it should fetch a planet named Mars', (done) => {
             let payload = {
                 id: 4
@@ -85,7 +85,7 @@ describe('Planets API Suite', () => {
               });
         });
 
-        it('it should fetch a planet named Satrun', (done) => {
+        it('it should fetch a planet named Saturn', (done) => {
             let payload = {
                 id: 6
             }
@@ -101,90 +101,4 @@ describe('Planets API Suite', () => {
         });
 
         it('it should fetch a planet named Uranus', (done) => {
-            let payload = {
-                id: 7
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(7);
-                    res.body.should.have.property('name').eql('Uranus');
-                done();
-              });
-        });
-
-        it('it should fetch a planet named Neptune', (done) => {
-            let payload = {
-                id: 8
-            }
-          chai.request(server)
-              .post('/planet')
-              .send(payload)
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('id').eql(8);
-                    res.body.should.have.property('name').eql('Neptune');
-                done();
-              });
-        });
-
-        // it('it should fetch a planet named Pluto', (done) => {
-        //     let payload = {
-        //         id: 9
-        //     }
-        //   chai.request(server)
-        //       .post('/planet')
-        //       .send(payload)
-        //       .end((err, res) => {
-        //             res.should.have.status(200);
-        //             res.body.should.have.property('id').eql(9);
-        //             res.body.should.have.property('name').eql('Sun');
-        //         done();
-        //       });
-        // });
-
-
-    });        
-});
-
-//Use below test case to achieve coverage
-describe('Testing Other Endpoints', () => {
-
-    describe('it should fetch OS Details', () => {
-        it('it should fetch OS details', (done) => {
-          chai.request(server)
-              .get('/os')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                done();
-              });
-        });
-    });
-
-    describe('it should fetch Live Status', () => {
-        it('it checks Liveness endpoint', (done) => {
-          chai.request(server)
-              .get('/live')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('status').eql('live');
-                done();
-              });
-        });
-    });
-
-    describe('it should fetch Ready Status', () => {
-        it('it checks Readiness endpoint', (done) => {
-          chai.request(server)
-              .get('/ready')
-              .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.have.property('status').eql('ready');
-                done();
-              });
-        });
-    });
-
-});
+            l
